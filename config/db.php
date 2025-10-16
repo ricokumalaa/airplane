484baa9de4c90489144bc6778a6824b08574eb79
ricokumalaa <130547060+ricokumalaa@users.noreply.github.com>
2025-10-16 13:11:38 +0700

    remove db

--- config/db.php
+++ config/db.php
@@ -5,8 +5,8 @@ return [
     'attributes' => [
             PDO::ATTR_STRINGIFY_FETCHES => true,
     ],
-    'dsn' => 'oci:dbname=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=202.158.123.94)(PORT=1521))(CONNECT_DATA=(SID=linux64)(SERVER=DEDICATED)))',
-    'username' => 'training',
-    'password' => 'training123',
+    'dsn' => 'oci:dbname=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=24)(PORT=1))(CONNECT_DATA=(SID=linux64)(SERVER=DEDICATED)))',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8'
 ];
